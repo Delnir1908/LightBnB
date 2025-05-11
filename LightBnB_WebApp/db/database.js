097937884225f9854b6036517861569b42bab272
Jason Chen <windson7@gmail.com>
2025-05-11 15:32:16 -0700

    refactor code for getUserWithEmail getUserWithId and addUser

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -1,5 +1,17 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "jasonchen",
+  password: "development",
+  host: "localhost",
+  database: "lightbnb",
+});
+
+//test code for checking connection
+// the following assumes that you named your connection variable `pool`
+// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
 
 /// Users
 
@@ -7,25 +19,59 @@ const users = require("./json/users.json");
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
- */
+*/
 const getUserWithEmail = function (email) {
+  /*
   let resolvedUser = null;
   for (const userId in users) {
     const user = users[userId];
     if (user && user.email.toLowerCase() === email.toLowerCase()) {
       resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+      }
+      }
+      return Promise.resolve(resolvedUser);
+      */
+     
+  return pool
+  .query(
+    `SELECT * FROM users WHERE email =  $1`, [email.toLowerCase()])
+    .then((result) => {
+      //if not user found, the result rows array should have no element, hence .length should === 0
+      if (result.rows.length === 0) {
+        return null;
+      }
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+          
 };
-
+      
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
- */
+*/
 const getUserWithId = function (id) {
+  /*
   return Promise.resolve(users[id]);
+  */
+  
+  return pool
+  .query(
+    `SELECT * FROM users WHERE id =  $1`, [id])
+    .then((result) => {
+      //if not user found, the result rows array should have no element, hence .length should === 0
+      if (result.rows.length === 0) {
+        return null;
+      }
+      return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 };
 
 /**
@@ -34,10 +80,27 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
+  /*
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
   return Promise.resolve(user);
+  */
+  return pool
+  .query(
+    `INSERT INTO users (name, email, password) 
+    VALUES ($1, $2, $3)
+    RETURNING *;`, [user.name, user.email, user.password])
+    .then((result) => {
+      return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+  
+
+
+
 };
 
 /// Reservations
@@ -59,12 +122,19 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+
+const getAllProperties = (options, limit = 10) => {
+
+  return pool
+    .query(
+      `SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
