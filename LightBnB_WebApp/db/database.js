b7d3bfeadb6063b38f3e95524d9cfe717f7c44eb
Jason Chen <windson7@gmail.com>
2025-05-11 17:41:03 -0700

    complete addProperty function

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -44,6 +44,7 @@ const getUserWithEmail = function (email) {
     })
     .catch((err) => {
       console.log(err.message);
+      throw err;
     });
           
 };
@@ -70,6 +71,7 @@ const getUserWithId = function (id) {
   })
   .catch((err) => {
     console.log(err.message);
+    throw err;
   });
 
 };
@@ -96,6 +98,7 @@ const addUser = function (user) {
   })
   .catch((err) => {
     console.log(err.message);
+    throw err;
   });
   
 
@@ -124,11 +127,13 @@ const getAllReservations = function (guest_id, limit = 10) {
     ORDER BY start_date
     LIMIT $2;`, [guest_id, limit])
   .then((result) => {
-    console.log(result.rows);
+    //test out result
+    // console.log(result.rows);
     return result.rows;
   })
   .catch((err) => {
     console.log(err.message);
+    throw err;
   });
 
 };
@@ -202,16 +207,18 @@ const getAllProperties = (options, limit = 10) => {
     `;
 
   // test out parameters
-  console.log(queryString, queryParams);
+  //console.log(queryString, queryParams);
 
   return pool
     .query(queryString, queryParams)
     .then((result) => {
-      console.log(result.rows);
+      //test out result
+      //console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
       console.log(err.message);
+      throw err;
     });
 
 };
@@ -222,10 +229,26 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
+  /*
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
   return Promise.resolve(property);
+  */
+  return pool
+  .query(
+    `INSERT INTO properties (owner_id,title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+    VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, $12, $13)
+    RETURNING *;`, [property.owner_id,title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .then((result) => {
+      return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+    throw err;
+  });
+
+
 };
 
 module.exports = {
